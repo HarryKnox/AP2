33e538dd7f290de573e54c502227db3b11a48eab
HarryKnox <knox-h@ulster.ac.uk>
2022-02-26 00:50:14 +0000

    Upload UI progress

--- ap2/src/app/upload/upload.page.ts
+++ ap2/src/app/upload/upload.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faRunning } from '@fortawesome/free-solid-svg-icons';
+import { bindCallback } from 'rxjs';
 
 @Component({
   selector: 'app-upload',
@@ -8,11 +9,21 @@ import { faRunning } from '@fortawesome/free-solid-svg-icons';
 })
 export class UploadPage implements OnInit {
 
+  // sets running icon from fortawesome import
   runIcon = faRunning;
+
+  // exercise type icons border colour
+  iconBorderColour = "0.01em solid black";
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  changeIconColour() {
+    this.iconBorderColour = "0.15em solid green";
   }
 
 }
