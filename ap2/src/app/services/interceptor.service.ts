9250f488e0ea8c599335c091237768c7127bdd2a
HarryKnox <knox-h@ulster.ac.uk>
2022-04-13 19:22:20 +0100

    Added comment deletion, when post is deleted.

--- ap2/src/app/services/interceptor.service.ts
+++ ap2/src/app/services/interceptor.service.ts
@@ -3,41 +3,37 @@ import {
   HttpRequest,
   HttpHandler,
   HttpInterceptor,
-} from '@angular/common/http'
-import { from} from 'rxjs';
+} from '@angular/common/http';
+import { from } from 'rxjs';
 import { AuthService } from './auth.service';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InterceptorService implements HttpInterceptor {
+  constructor(private auth: AuthService) {}
 
-  constructor(
-    private auth : AuthService) { }
+  // Code Reference https://stackoverflow.com/a/57712118
 
   // func that intercepts each request, calls handle func
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     // convert promise to observable using 'from' operator
-    return from(this.handle(req, next))
+    return from(this.handle(req, next));
   }
 
   // func to handle the HTTP requests
   async handle(req: HttpRequest<any>, next: HttpHandler) {
-
     // gets the token
-    let authToken = await this.auth.getToken()
+    let authToken = await this.auth.getToken();
 
     // sets token in Auth. Header
     const authReq = req.clone({
       setHeaders: {
-        Authorization: "Bearer "+authToken
-      }
-    })
+        Authorization: 'Bearer ' + authToken,
+      },
+    });
 
     // returns token, as a promise
-    return next.handle(authReq).toPromise()
+    return next.handle(authReq).toPromise();
   }
-
 }
-
